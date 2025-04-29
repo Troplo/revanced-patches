f821612041e7ef4013f3840a745782ba4dc171d3
Troplo <troplo@troplo.com>
2025-04-29 13:06:34 +1000

    Update UnlockDuolingoSuperPatch.kt

--- patches/src/main/kotlin/app/revanced/patches/duolingo/unlocksuper/UnlockDuolingoSuperPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/duolingo/unlocksuper/UnlockDuolingoSuperPatch.kt
@@ -2,12 +2,11 @@ package app.revanced.patches.duolingo.unlocksuper
 
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
 import app.revanced.patcher.patch.bytecodePatch
+import app.revanced.patcher.extensions.BytecodeContextExtensions.findResult
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.builder.instruction.BuilderInstruction22c
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.reference.Reference
 import app.revanced.patches.duolingo.unlocksuper.fingerprints.IsUserSuperMethodFingerprint
 import app.revanced.patches.duolingo.unlocksuper.fingerprints.UserSerializationMethodFingerprint
 import app.revanced.patcher.patch.PatchException
@@ -17,34 +16,30 @@ val unlockDuolingoSuperPatch = bytecodePatch(
     name = "Unlock Duolingo Super",
     use = false,
 ) {
-    compatibleWith("com.duolingo"("5.158.4")) // You can update the version accordingly.
+    compatibleWith("com.duolingo"("5.158.4")) // update version if needed
 
     execute {
-        val isUserSuperReference = IsUserSuperMethodFingerprint.result?.mutableMethod
-            ?.getInstructions()
-            ?.filterIsInstance<BuilderInstruction22c>()
-            ?.firstOrNull { it.opcode == Opcode.IGET_BOOLEAN }
+        val isUserSuperMethod = findResult(IsUserSuperMethodFingerprint)
+        val userSerializationMethod = findResult(UserSerializationMethodFingerprint)
+
+        val isUserSuperReference = isUserSuperMethod.mutableMethod.instructions
+            .filterIsInstance<BuilderInstruction22c>()
+            .firstOrNull { it.opcode == Opcode.IGET_BOOLEAN }
             ?.reference
             ?: throw IsUserSuperMethodFingerprint.exception
 
-        UserSerializationMethodFingerprint.result?.mutableMethod?.apply {
-            val assignIndex = findAssignIndex(isUserSuperReference)
-            val assignInstruction = getInstruction<TwoRegisterInstruction>(assignIndex)
+        val assignIndex = userSerializationMethod.mutableMethod.instructions.indexOfFirst { 
+            it is BuilderInstruction22c && it.opcode == Opcode.IPUT_BOOLEAN && it.reference == isUserSuperReference
+        }.takeIf { it != -1 } ?: throw PatchException("Could not find assignment of isUserSuper.")
 
-            addInstructions(
-                assignIndex + 1,
-                """
-                    const/4 v${assignInstruction.registerA}, 0x1
-                    iput-boolean v${assignInstruction.registerA}, v0, $isUserSuperReference
-                """.trimIndent()
-            )
-        } ?: throw UserSerializationMethodFingerprint.exception
+        val assignInstruction = userSerializationMethod.mutableMethod.getInstruction<TwoRegisterInstruction>(assignIndex)
+
+        userSerializationMethod.mutableMethod.addInstructions(
+            assignIndex + 1,
+            """
+                const/4 v${assignInstruction.registerA}, 0x1
+                iput-boolean v${assignInstruction.registerA}, v0, $isUserSuperReference
+            """.trimIndent()
+        )
     }
-}
-
-private fun findAssignIndex(reference: Reference): Int {
-    return UserSerializationMethodFingerprint.result?.mutableMethod?.getInstructions()
-        ?.indexOfFirst { it is BuilderInstruction22c && it.opcode == Opcode.IPUT_BOOLEAN && it.reference == reference }
-        ?.takeIf { it != -1 }
-        ?: throw PatchException("Could not find index of instruction with supplied reference.")
 }
