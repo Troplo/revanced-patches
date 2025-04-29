d8295a46fb77f33130ba9b022397761affe89d81
Troplo <troplo@troplo.com>
2025-04-29 13:06:49 +1000

    Update IsUserSuperMethodFingerprint.kt

--- patches/src/main/kotlin/app/revanced/patches/duolingo/unlocksuper/fingerprints/IsUserSuperMethodFingerprint.kt
+++ patches/src/main/kotlin/app/revanced/patches/duolingo/unlocksuper/fingerprints/IsUserSuperMethodFingerprint.kt
@@ -1,21 +1,17 @@
 package app.revanced.patches.duolingo.unlocksuper.fingerprints
 
 import app.revanced.patcher.extensions.or
-import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patcher.fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
-internal object IsUserSuperMethodFingerprint : MethodFingerprint(
-    returnType = "Ljava/lang/Object",
-    parameters = listOf(
-        "Ljava/lang/Object",
-        "Ljava/lang/Object",
-    ),
-    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
-    strings = listOf(
-        "user",
-        "heartsState",
-        "superData",
-    ),
-    opcodes = listOf(Opcode.IGET_BOOLEAN),
-)
+internal val IsUserSuperMethodFingerprint = fingerprint {
+    accessFlags(AccessFlags.PUBLIC or AccessFlags.FINAL)
+    returnType("Ljava/lang/Object;")
+    parameters(
+        "Ljava/lang/Object;",
+        "Ljava/lang/Object;"
+    )
+    strings("user", "heartsState", "superData")
+    opcodes(Opcode.IGET_BOOLEAN)
+}
